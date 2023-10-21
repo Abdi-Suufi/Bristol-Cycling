2c01a05feb853130b6dd7117cf863e1bd5cdb722
Abdi-Suufi <abdirahman2.suufi@live.uwe.ac.uk>
2023-10-21 21:26:39 +0100

    minor update
    table styled but no info from sql yet

--- database.js
+++ database.js
@@ -4,6 +4,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
     host: '127.0.0.1',
     user: 'root',
+    port: '3306',
     password: '',
     database: 'cycle_shop',
 });
