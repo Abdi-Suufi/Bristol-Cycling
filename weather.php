354fe5096cba32f327c83f89e9ce12c1183a72ee
Abdi-Suufi <abdirahman2.suufi@live.uwe.ac.uk>
2023-12-04 03:59:20 +0000

    Completed weather implementation on new page
    Can look cleaner but cba

--- weather.php
+++ weather.php
@@ -9,10 +9,19 @@
     <script src="https://kit.fontawesome.com/56e72382bd.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="styles.css">
     <script src="assets/js/navbar.js"></script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Arial">
 </head>
+<style>
+    body {
+        font-family: arial;
+    }
 
-<body style="background: rgb(0,0,0);
-background: linear-gradient(90deg, rgba(0,0,0,1) 0%, rgba(11,11,11,1) 7%, rgba(45,45,45,1) 52%, rgba(17,17,17,1) 87%, rgba(0,0,0,1) 100%);">
+    #table-test {
+        max-width: 200px;
+        margin: 0 auto;
+    }
+</style>
+<body style="background: rgb(0,0,0); background: linear-gradient(90deg, rgba(0,0,0,1) 0%, rgba(11,11,11,1) 7%, rgba(45,45,45,1) 52%, rgba(17,17,17,1) 87%, rgba(0,0,0,1) 100%); ">
     <nav class="navbar navbar-expand-md fixed-top" id="mainNav">
         <div class="container">
             <a class="navbar-brand" href="index.php">Bristol Cycling</a>
@@ -27,18 +36,15 @@ background: linear-gradient(90deg, rgba(0,0,0,1) 0%, rgba(11,11,11,1) 7%, rgba(4
         </div>
     </nav>
     <div class="container mt-5"><br>
-
-    <div class="card text-white bg-dark mb-3">
+    <div class="card text-white bg-dark mb-3 text-center"id="table-test">
             <div class="card-header">Current Weather</div>
             <div class="card-body">
-                <h5 class="card-title">Bristol, UK</h5>
                 <p class="card-text">Temperature: <span id="currentTemp"></span>°C</p>
                 <p class="card-text">Weather: <span id="currentWeather"></span></p>
                 <p class="card-text">Humidity: <span id="currentHumidity"></span>%</p>
                 <p class="card-text">Wind Speed: <span id="currentWindSpeed"></span> m/s</p>
             </div>
         </div>
-    
         <h2>Weather Forecast for Bristol, UK</h2>
         <div class="table-responsive">
             <table class="table table-striped table-dark">
@@ -62,7 +68,29 @@ background: linear-gradient(90deg, rgba(0,0,0,1) 0%, rgba(11,11,11,1) 7%, rgba(4
         // Your OpenWeatherMap API key
         const apiKey = '2645d3a34171a029a0ec6d4265529d9a';
 
+        // Function to display current weather
+        function displayCurrentWeather(currentWeather) {
+            document.getElementById('currentTemp').innerText = currentWeather.main.temp;
+            document.getElementById('currentWeather').innerText = currentWeather.weather[0].description;
+            document.getElementById('currentHumidity').innerText = currentWeather.main.humidity;
+            document.getElementById('currentWindSpeed').innerText = currentWeather.wind.speed;
+        }
+
+        // Function to fetch current weather
+        async function getCurrentWeather() {
+            try {
+                const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Bristol&appid=${apiKey}&units=metric`);
+                const currentWeatherData = await response.json();
+
+                // Display current weather in the card
+                displayCurrentWeather(currentWeatherData);
+            } catch (error) {
+                console.log('Error fetching current weather:', error);
+                // Handle error if needed
+            }
+        }
         // Function to fetch weather forecast for the next 7 days
+        
         async function getWeatherForecast() {
             try {
                 const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=Bristol&cnt=8&appid=${apiKey}&units=metric`);
@@ -89,6 +117,9 @@ background: linear-gradient(90deg, rgba(0,0,0,1) 0%, rgba(11,11,11,1) 7%, rgba(4
                 weatherTable.innerHTML = '<tr><td colspan="5">Failed to fetch weather forecast</td></tr>';
             }
         }
+
+        // Call the function to get current weather on page load
+        getCurrentWeather();
 
         // Call the function to get weather forecast on page load
         getWeatherForecast();
